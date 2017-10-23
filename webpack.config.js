84808586a6f533030b7e79731b014c57c7a508f6
readerxiao <1014784938@qq.com>
2017-10-23 17:48:02 +0800

    secend commit

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-
-module.exports = {  //这是commonJS的导出语法
-    entry: './app/index.js', //entry就是我们的入口文件，可以有多个入口文件,是webpack打包的输入对象
-    output: { //output即为webpack打包的输出对象
-        filename: './dist/bundle.js',//filename为输出文件名
-    },
-}
